78a1decda1cfd7657d00bd0b177781dff39d6b9a
Adam Gibson <adamsteven15@gmail.com>
2021-08-04 22:31:00 +0100

    Implemented Solve function

--- main.cpp
+++ main.cpp
@@ -4,38 +4,41 @@
 
 using namespace std;
 
-vector<vector<char>> grid = {
-    {'8', '6', '3', '9', '5', '1', '.', '.', '.'},
-    {'.', '9', '.', '.', '.', '.', '5', '.', '1'},
-    {'.', '1', '.', '.', '.', '.', '.', '.', '.'},
-    {'.', '2', '6', '4', '1', '.', '.', '5', '.'},
-    {'.', '.', '.', '.', '.', '7', '.', '1', '.'},
-    {'1', '.', '.', '.', '8', '6', '.', '.', '9'},
-    {'6', '3', '.', '.', '7', '2', '.', '.', '.'},
-    {'9', '.', '2', '.', '.', '.', '.', '6', '.'},
-    {'.', '.', '.', '6', '.', '3', '.', '2', '8'},
+vector<vector<int>> grid = {
+    {8, 6, 3, 9, 5, 1, 0, 0, 0},
+    {0, 9, 0, 0, 0, 0, 5, 0, 1},
+    {0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 2, 6, 4, 1, 0, 0, 5, 0},
+    {0, 0, 0, 0, 0, 7, 0, 1, 0},
+    {1, 0, 0, 0, 8, 6, 0, 0, 9},
+    {6, 3, 0, 0, 7, 2, 0, 0, 0},
+    {9, 0, 2, 0, 0, 0, 0, 6, 0},
+    {0, 0, 0, 6, 0, 3, 0, 2, 8},
 };
 
 bool possible(int x, int y, int n)
 {
-    int temp;
-
-    if (grid[x][y] != '.')
-        return false;
-
     for (size_t gridX = 0; gridX < 9; gridX++)
     {
-        if ((int)grid[gridX][y] == n)
-        {
+        if (grid[gridX][y] == n)
             return false;
-        }
     }
 
     for (size_t gridY = 0; gridY < 9; gridY++)
     {
-        if ((int)grid[x][gridY] == n)
-        {
+        if (grid[x][gridY] == n)
             return false;
+    }
+
+    int innerX = (x / 3) * 3;
+    int innerY = (y / 3) * 3;
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            if (grid[innerX + i][innerY + j] == n)
+                return false;
         }
     }
 
@@ -44,6 +47,37 @@ bool possible(int x, int y, int n)
 
 void solve()
 {
+    for (size_t x = 0; x < 9; x++)
+    {
+        for (size_t y = 0; y < 9; y++)
+        {
+            if (grid[x][y] == 0)
+            {
+                for (size_t i = 1; i < 10; i++)
+                {
+                    if (possible(x, y, i))
+                    {
+                        grid[x][y] = i;
+                        solve();
+                        grid[x][y] = 0;
+                    }
+                }
+
+                return;
+            }
+        }
+    }
+
+    cout << "\n";
+    cout << "\n";
+    for (size_t x = 0; x < 9; x++)
+    {
+        for (size_t y = 0; y < 9; y++)
+        {
+            cout << grid[x][y] << ", ";
+        }
+        cout << "\n";
+    }
 }
 
 int main()
@@ -56,6 +90,8 @@ int main()
         }
         cout << "\n";
     }
+
+    solve();
 
     return 0;
 }
